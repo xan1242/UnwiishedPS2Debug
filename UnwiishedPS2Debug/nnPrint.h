1d5923dc85f4f02cb40d33aef605c32ee6f9bf03
xan1242 <8014093+xan1242@users.noreply.github.com>
2026-01-12 04:50:45 +0100

    expose nnPrint

--- UnwiishedPS2Debug/nnPrint.h
+++ UnwiishedPS2Debug/nnPrint.h
@@ -6,4 +6,7 @@
 void nnPrint_Init();
 void nnPrint_InitDebugPrint(int sizeX, int sizeY);
 
+extern void(*nnSetPrintColor)(unsigned int color);
+extern void(*nnPrint)(int x, int y, const char* fmt, ...);
+
 #endif
